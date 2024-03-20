7cac331469c1b92f17c86a6c70f5ffad376c8b00
waffle <nate.crema@gmail.com>
2024-03-20 23:04:58 +0900

    feat: 1차 및 2차 교육 디렉토리구조 수정 및 일부자료 추가
    - 폴더구조 변경:
        - 과제: homeworks
        - 예제코드: lectures/examples
        - 미니프로젝트: lectures/projects
    - 신규코드 추가:
        - 연산자 관련코드 -> examples
        - 미니프로젝트 추가: SIMPE_EVERYTIME -> projects

---
+++ EDU_EXAMPLE/2_CONDITIONAL_AND_LOOP_STATEMENTS/projects/prob.star2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    for ( int i = 0; i < 5; i++ ) {
+        for ( int j = 5; j > i+1; j-- )
+            printf(" ");
+        for ( int k = 0; k <= i; k++ )
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
